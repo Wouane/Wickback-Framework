c51640e10b83aa2eb5a1590c355b4af0023980c7
Florian Wouane <pro.florian.michel@gmail.com>
2015-08-19 10:29:29 +0200

    add error layout + picture

--- app/templates/w_errors/404.php
+++ app/templates/w_errors/404.php
@@ -1,5 +1,7 @@
-<?php $this->layout('layout', ['title' => 'Perdu ?']) ?>
+<?php $this->layout('error_layout', ['title' => 'ERROR 404 :: Vous êtes Perdu ?']) ?>
 
 <?php $this->start('main_content'); ?>
 <h1>404. Perdu ?</h1>
+<img src="<?php echo $this->assetUrl('img/peugeot404.jpg'); ?>" alt="image404"/>
+
 <?php $this->stop('main_content'); ?>
