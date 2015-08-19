b20b1b9bd0ef5fc0ab36ed7e024128dc0f2c7e70
Florian Wouane <pro.florian.michel@gmail.com>
2015-08-19 15:37:49 +0200

    function GetCurrentWordofTheDay

---
+++ app/Manager/TermManager.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Manager;
+
+	class TermManager extends \W\Manager\Manager
+	{	
+		/**
+		*Récupère le mot du jour actuel
+		*/
+		public function getCurrentWordOfTheDay()
+		{
+			
+			$sql = "SELECT *
+					FROM  $this->table
+					WHERE is_wotd = 1";
+
+			$sth = $this->dbh->prepare($sql);
+			$sth->execute();
+
+			return $sth->fetch();
+		}
+	}
