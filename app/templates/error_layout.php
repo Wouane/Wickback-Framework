bb24b2fc514401f99ab5a1410254e2d66d428c4a
Florian Wouane <pro.florian.michel@gmail.com>
2015-08-20 09:53:33 +0200

    add error layout

---
+++ app/templates/error_layout.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title><?= $this->e($title) ?></title>
+</head>
+<body>
+	<div class="container">
+		<section>
+			<?= $this->section('main_content') ?>
+		</section>
+	</div>
+</body>
+</html>
