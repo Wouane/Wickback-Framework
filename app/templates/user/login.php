81f68a71912a4815d7fddfd0dad7e2390ffe7935
Florian Wouane <pro.florian.michel@gmail.com>
2015-08-21 15:19:44 +0200

    add login.php

--- app/templates/user/login.php
+++ app/templates/user/login.php
@@ -1,23 +1,19 @@
-<?php $this->layout('layout', ['title' => 'Login d\'un admin']) ?>
+<?php $this->layout('layout', ['title' => 'Connexion d\'un admin']) ?>
 
 <?php $this->start('main_content') ?>
 	
-	<h2>Login administrateur</h2>
-
-		<p class="login_admin">Veuillez renseigner ci-dessous votre mot de passe et login administrateur</p>
+	<h2>Connexion administrateur</h2>
 
 		<form method="POST" novalidate>
-			<label for="email">Email</label>
-			<input type="email" id="email" name="email" value="<?= $email ?>"><br />	
+			<label for="username">Pseudo</label>
+			<input type="text" id="username" name="username" value=""><br />	
 
 			<label for="paswword">Mot de passe</label>	
 			<input type="password" id="password" name="password" value=""><br />	
 
-			<label for="password_confirm">Mot de passe encore</label>
-			<input type="password" id="password_confirm" name="password_confirm" value="">
-			<br />	
 			<input type="submit" value="Go">
 			<input type="reset" value="Reset">
 			<div class="error"><?= $error ?></div>
 		</form>
+
 <?php $this->stop('main_content') ?>
