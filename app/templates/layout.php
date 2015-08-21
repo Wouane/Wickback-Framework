6d823e6d0231006bc7dc82007fdfe3a4a8af7ae1
Florian Wouane <pro.florian.michel@gmail.com>
2015-08-21 09:58:11 +0200

    delete nav add bootstrap

--- app/templates/layout.php
+++ app/templates/layout.php
@@ -5,6 +5,7 @@
 	<title><?= $this->e($title) ?></title>
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/reset.css') ?>">
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/style.css') ?>">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 	<link href='http://fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
 
 </head>
@@ -12,8 +13,7 @@
 	<div class="container">
 		<header>
 			<h1>W :: <?= $this->e($title) ?></h1>
-			<nav>
-			</nav>
+					
 		</header>
 
 		<section>
