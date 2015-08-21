8bae413ed9d5a863bfd1762547c1eba4b2a7c0a4
Florian Wouane <pro.florian.michel@gmail.com>
2015-08-21 10:06:57 +0200

    login administrateur

---
+++ app/templates/user/login.php
@@ -0,0 +1,23 @@
+<?php $this->layout('layout', ['title' => 'Login d\'un admin']) ?>
+
+<?php $this->start('main_content') ?>
+	
+	<h2>Login administrateur</h2>
+
+		<p class="login_admin">Veuillez renseigner ci-dessous votre mot de passe et login administrateur</p>
+
+		<form method="POST" novalidate>
+			<label for="email">Email</label>
+			<input type="email" id="email" name="email" value="<?= $email ?>"><br />	
+
+			<label for="paswword">Mot de passe</label>	
+			<input type="password" id="password" name="password" value=""><br />	
+
+			<label for="password_confirm">Mot de passe encore</label>
+			<input type="password" id="password_confirm" name="password_confirm" value="">
+			<br />	
+			<input type="submit" value="Go">
+			<input type="reset" value="Reset">
+			<div class="error"><?= $error ?></div>
+		</form>
+<?php $this->stop('main_content') ?>
