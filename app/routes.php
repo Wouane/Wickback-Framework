b295dfbd7614e8e249da0a3f491a0d1149a63ebf
Florian Wouane <pro.florian.michel@gmail.com>
2015-08-21 09:58:01 +0200

    Add Routes Back Office Admin

--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,18 @@
 <?php
 	
 	$w_routes = array(
-		['GET', '/', 'Default#home', 'home'],
+		// ------------------------ADMIN---------------------------------------------------------------
+		//accueil de l'admin --------------------------------------------------------------------------
+		['GET', '/admin/termes/', 'Term#showAll', 'show_all_terms'],
+		// efface un terme ----------------------------------------------------------------------------
+		['GET', '/admin/termes/suppression/[i:id]/', 'Term#delete', 'delete_term'],
+		// modifie un terme ---------------------------------------------------------------------------
+		['GET|POST', '/admin/termes/modification/[i:id]/', 'Term#edit', 'edit_term'],
+		// changer le mot du jour ---------------------------------------------------------------------
+		['GET', '/admin/termes/nouveau-mdj/[i:id]/', 'Term#changeWotd', 'change_wotd'],
+		// Creer un nouvel administrateur -------------------------------------------------------------
+		['GET|POST', '/admin/administrateurs/inscription/', 'User#register', 'register_administrator'],
+		// connexion login ----------------------------------------------------------------------------
+		['GET|POST', '/admin/login/', 'User#login', 'login'],
+		//----------------------------------------------------------------------------		
 	);
